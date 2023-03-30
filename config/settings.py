1f8535eaa49bd065e637da81b9f854a3a3971e17
fctnvl77 <fctnvlwind@gmail.com>
2023-03-30 11:46:05 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!zvjpde2esd(tlhywh_)4&oo3d1e--+16)710=%o6@39k*c#+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -115,8 +115,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# if DEBUG:
+#     STATICFILES_DIRS =[
+#         BASE_DIR /'static'
+#     ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
